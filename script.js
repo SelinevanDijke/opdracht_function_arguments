8270068881db97959fd5849c5be4668782002831
Seline <74591087+SelinevanDijke@users.noreply.github.com>
2021-07-06 20:41:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const paintWall = function (muur, verf) {
+    console.log("De " + muur + " muur is " + verf + " geverfd.");
+};
+
+const kantMuur = "rechter";
+const kleurVerf = "blauw";
+
+paintWall(kantMuur, kleurVerf);
+
+// Wanneer er geen argument toe wordt gevoegd wordt er geen kleur getoond. 
+// De volgorde maakt uit wanneer de functie wordt opgeroepen.
+//  Wanneer je de argumenten switcht komen ze andersom te staan
+
+
